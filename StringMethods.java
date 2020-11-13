6c3f2b62f511f11855374a7d66788bf54bca18d8
shubham409 <sm091274@gmail.com>
2020-11-14 01:57:32 +0530

    String methods

---
+++ StringMethods.java
@@ -0,0 +1,29 @@
+public class StringMethods {
+    
+}
+// (Note that Strings are immutable so it will create new objects in heap and a copy of that in the pool )
+// 1.String toUpperCase()
+// 2.String toLowerCase()
+// 3.String trim()
+
+// 4.substring()
+// 5.substring(strting_index,ending_index)
+
+// 6.String replace(old_char,new_char)
+
+// 7.boolean startsWith(string)
+// 8.boolean endsWith(string)
+
+// 9.char charAt(index)
+
+// 10.int indexOf(string)--------------->     left to right searching
+// 10.int indexOf(string,start_index)
+
+// 11.int lastIndexOF(string) right to left searching <--------------
+// 11.int lastIndexOF(string,start_index)
+
+// 12.boolean equals(string)
+// 13.boolean equalsIgnoreCase(string)
+
+// 14.int compareTo(string)
+// 15.String valueOf(int)
